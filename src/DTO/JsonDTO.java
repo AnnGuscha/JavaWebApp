e2517a2e4b3d8a63507ba6852ef655db3a4efaca
Guscha Anna <ann9510.as@gmail.com>
2015-12-11 21:01:30 +0300

    add json

--- src/DTO/JsonDTO.java
+++ src/DTO/JsonDTO.java
@@ -4,46 +4,11 @@ package DTO;
  * Created by Anna on 12/11/2015.
  */
 public class JsonDTO {
-    public String sEcho
-    public string sSearch
-    public int iDisplayLength
-    public int iDisplayStart
-    public int iColumns
-    public int iSortingCols
-    public string sColumns
-
-    {
-        get;
-        set;
-    }
-
-    {
-        get;
-        set;
-    }
-
-    {
-        get;
-        set;
-    }
-
-    {
-        get;
-        set;
-    }
-
-    {
-        get;
-        set;
-    }
-
-    {
-        get;
-        set;
-    }
-
-    {
-        get;
-        set;
-    }
+    public String sEcho;
+    public String sSearch;
+    public int iDisplayLength;
+    public int iDisplayStart;
+    public int iColumns;
+    public int iSortingCols;
+    public String sColumns;
 }
